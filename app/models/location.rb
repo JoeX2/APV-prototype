8dfb08b684cd030417e32aef8192e2e5cb911600
Joe Sørensen <joe.sorensen@gmail.com>
2014-05-26 23:51:20 +0200

    Added materials to locations view

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base
   has_and_belongs_to_many :materials
+  accepts_nested_attributes_for :materials
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -1,3 +1,4 @@
 class Material < ActiveRecord::Base
   has_and_belongs_to_many :locations
+  accepts_nested_attributes_for :locations
 end
